a63230a8e5d1c5a9b95dbc7f7f1125977076b416
Riya Rastogi <riya02rastogi@gmail.com>
2025-05-18 20:02:27 +0530

    completed

---
+++ src/assets/index.js
@@ -0,0 +1,104 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import ircs from "./company/ircs.png";
+import bits from "./company/bits.png";
+import tesla from "./company/tesla.png";
+import React_shop from "./React_shop.png";
+import LitedIn from "./litedin.png";
+
+import carrent from "./carrent.png";
+import NCL from "./CSIR-NCL_Logo.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import Real_img from "./Real_img.png";
+import quiz from "./quiz.png";
+import gallery from "./gallery.png";
+import BLOOD from "./ircs_deployed.png";
+
+import nextjs from "./tech/nextjs.png";         
+import Real_Estate from "./Real_Estate.png";
+import Codeup_img from "./Codeup_img.png";
+import Socially_img from "./Socially_img.png";
+import Pingup_img from "./Pingup_img.png";
+import Csir_logo from "./company/Csir_logo.png";
+import Indus_logo from "./company/Indus_logo.png";
+import Hospital_img from "./Hospital_img.png";
+import sql from "./tech/sql.png";
+import Car_img from "./Car_img.png";
+import Amity from "./company/Amity.png";
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  ircs,
+  tesla,
+  carrent,
+  jobit,
+  bits,
+  BLOOD,
+  quiz,
+  gallery,
+  React_shop,
+  Real_img,
+  tripguide,
+  NCL,
+  LitedIn,
+  nextjs,
+  Real_Estate,
+  Codeup_img,
+  Socially_img,
+  Pingup_img,
+  Csir_logo,
+  Indus_logo,
+  Hospital_img,
+  sql,
+  Car_img,
+  Amity,
+};
